ad6052d615c6fb5d6fd4f4fbd2ae2f6a9e6f52bb
Octe Reviyanto Nugroho <octe@tabsquare.com>
2020-08-07 19:13:27 +0700

    update

--- src/index.js
+++ src/index.js
@@ -14,6 +14,9 @@ app.use(express.static(path.resolve(__dirname, '../public')));
 app.get('/', function(req, res) {
     res.render('pages/index');
 });
+app.get('/video', function(req, res) {
+    res.render('pages/video');
+});
 
 app.listen(8080);
 console.log('8080 is the magic port');
